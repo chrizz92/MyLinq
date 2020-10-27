c973c66335e15e1b5de0f3a66d738ae24801a49e
chrizz92 <christianschadler@gmx.at>
2020-10-27 20:47:28 +0100

    Add project files.

---
+++ MyLinq/MyLinq.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLinq
+{
+    internal static class MyLinq
+    {
+        internal static List<int> MyWhere(List<int> numbers, Func<int, bool> isEven)
+        {
+            List<int> newList = new List<int>();
+            foreach (int number in numbers)
+            {
+                if (isEven(number))
+                {
+                    newList.Add(number);
+                }
+            }
+            return newList;
+        }
+
+        internal static List<T> MyWhere<T>(List<T> objects, Func<T, bool> isTrue)
+        {
+            List<T> newList = new List<T>();
+            foreach (T obj in objects)
+            {
+                if (isTrue(obj))
+                {
+                    newList.Add(obj);
+                }
+            }
+            return newList;
+        }
+
+        internal static IEnumerable<T> MyWhere<T>(IEnumerable<T> objects, Func<T, bool> isTrue)
+        {
+            List<T> newList = new List<T>();
+            foreach (T obj in objects)
+            {
+                if (isTrue(obj))
+                {
+                    newList.Add(obj);
+                }
+            }
+            return newList;
+        }
+    }
+}
---
+++ MyLinq/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyLinq
+{
+    internal class Program
+    {
+        public static bool isEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            List<int> newNumbers = MyLinq.MyWhere(numbers, isEven) as List<int>;
+            int[] myIntArray = new int[] { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            int[] myNewIntArray = MyLinq.MyWhere(myIntArray, isEven).ToArray();
+
+            foreach (var number in newNumbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            foreach (int myInt in myNewIntArray)
+            {
+                Console.WriteLine(myInt);
+            }
+        }
+    }
+}
